8e25117b6c68b7218eb742f5c54cc985068e9706
CesarVelasco23310357 <a23310357@live.ceti.mx>
2025-05-06 23:09:55 +0000

    Practica 5

--- bin/src/archivos.cpp
+++ bin/src/archivos.cpp
@@ -1,35 +1,35 @@
-using namespace std;
+#include <iostream>
+ #include <fstream>
+ #include <string>
  
- struct Persona
- {
- 	char nombre [12];
- 	int edad;
- };
+ using namespace std;
  
  int main(int argc, char const *argv[])
  {
  	ofstream archivoSalida("archivo.txt");
-  int main(int argc, char const *argv[])
- 	}
- 	archivoEntrada.close();
- 
- 	//------------------------------------------------------------------------------------
- 	// Escritura de archivos Binarios
- 	ofstream archivoSalidaBinario("archivo.bin",ios::binary);
- 	if (!archivoSalidaBinario)
+ 	if (!archivoSalida)
  	{
  		cerr << "Error al leer archivo" << endl;
  		return 1; // finalizar error
  	}
+ 	cout << "Escribiendo archivo" << endl;
+ 	archivoSalida << "Hola mundo este es mi primer archivo" << endl;
  
- 	Persona p1 = {"Juan", 20};
- 	Persona p2 = {"Pedro", 30};
- 	Persona p3 = {"Maria", 40};
+ 	archivoSalida.close();
  
- 	archivoSalidaBinario.write((char*)(&p1),sizeof(Persona));
- 	archivoSalidaBinario.write((char*)(&p2),sizeof(Persona));
- 	archivoSalidaBinario.write((char*)(&p3),sizeof(Persona));
+ 	ifstream archivoEntrada("archivo.txt");
+ 	if (!archivoEntrada)
+ 	{
+ 		cerr << "Error al leer archivo" << endl;
+ 		return 1; // finalizar error
+ 	}
+ 	cout << "Leyendo archivo" << endl;
+ 	string linea;
+ 	while (getline(archivoEntrada, linea))
+ 	{
+ 		cout << linea << endl;
+ 	}
+ 	archivoEntrada.close();
  
- 	archivoSalidaBinario.close();
  	return 0;
  }
